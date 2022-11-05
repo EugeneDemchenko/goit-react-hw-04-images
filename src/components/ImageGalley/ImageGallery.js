bc56b44a089bc730eedb1c80b8d55eadaf078746
EugeneDemchenko <108480374+EugeneDemchenko@users.noreply.github.com>
2022-11-05 22:05:49 +0200

    ground

---
+++ src/components/ImageGalley/ImageGallery.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import PropTypes from 'prop-types'
+import './ImageGallery.css'
+
+const ImageGallery = () => {
+    return (
+        <ul class="gallery">
+            <li class="gallery-item">
+                <img src="" alt="" className="gallery-image"/>
+            </li>
+        </ul>
+    )
+}
+
+export default ImageGallery
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Modal.css'
+
+const Modal = () => {
+    return (
+        <div class="overlay">
+            <div class="modal">
+                <img src="" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Modal
