232c1340224b4a81083895b6665ac8dada9bf22c
EugeneDemchenko <108480374+EugeneDemchenko@users.noreply.github.com>
2022-11-10 07:24:29 +0200

    fixe error in useEffect   +   clear

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,5 +1,4 @@
 import  { useState, useEffect } from "react";
-// import  React, { Component} from "react";
 import ImageGalleryItem from "../ImageGalleryItem/ImageGalleryItem";
 import Button from "../Button/Button";
 import Loader from "../Loader/Loader";
@@ -24,8 +23,8 @@ export default function ImageGallery({requestImg}) {
     
 
     useEffect(() => {
-        setIsLoading(true)
-        if (requestImg === searchQuery) {
+        function findImg() {
+            setIsLoading(true)
             fetch(`https://pixabay.com/api/?key=30341447-eb158986107312b6bdd4f8895&q=${searchQuery}&image_type=photo&page=${page}&per_page=12`)
                 .then(res => res.json())
                 .then(data => {
@@ -33,39 +32,10 @@ export default function ImageGallery({requestImg}) {
                     setTotalPages(Math.ceil(data.totalHits / 12));
                 })
                 .finally(() => setIsLoading(false))
-
-        
         }
-},[searchQuery, page])
+        searchQuery && findImg()
+    },[searchQuery, page])
     
-    
-    // useEffect ((prevProps, prevState) => {
-    //     if (prevProps.request !== searchQuery || prevState.page !== page) {
-    //         setIsLoading(true)
-    //             fetch(`https://pixabay.com/api/?key=30341447-eb158986107312b6bdd4f8895&q=${request}&image_type=photo&page=${page}&per_page=12`)
-    //                 .then(res => res.json())
-    //                 .then(data =>
-    //                     setArticles(prevState => (page === 1 ? data.hits : [...prevState, ...data.hits])),
-    //                     setTotalPages(Math.ceil(data.totalHits / 12))
-    //                 )
-    //                 .finally(() => setIsLoading(false))
-
-    //     } 
-    // },[])
-    
-    
-    
-    // useEffect(() => {
-    //     fetch(`https://pixabay.com/api/?key=30341447-eb158986107312b6bdd4f8895&q=${request}&image_type=photo&page=${page}&per_page=12`)
-    //         .then(res => res.json())
-    //         .then(data =>
-    //             setArticles(prevState => (page === 1 ? data.hits : [...prevState, ...data.hits])),
-    //             setTotalPages( Math.ceil(data.totalHits / 12) )
-    //         )
-    //         .finally(() => setIsLoading(false))
-
-    // },[])
-
     const loadMore = () => {
         setPage(prevState => prevState + 1)
         }
@@ -89,69 +59,6 @@ export default function ImageGallery({requestImg}) {
     )
 
 }
-
-
-// class ImageGallery extends Component {
-//     state = {
-//         articles: [],
-//         page: 1,
-//         isLoading: false,
-//         totalPages: 1,
-//         searchQuery: ''
-//     }
-//     static getDerivedStateFromProps (nextProps, state) {
-//         if (nextProps.searchQuery !== state.searchQuery) {
-//             return { page: 1, searchQuery: nextProps.searchQuery };
-//         }
-//         return state;
-//      }
-
-//     componentDidUpdate(prevProps, prevState) {
-//         if (prevProps.searchQuery !== this.props.searchQuery || prevState.page !== this.state.page) {
-//             this.setState({ isLoading: true })
-//             setTimeout(() => {
-//                 fetch(`https://pixabay.com/api/?key=30341447-eb158986107312b6bdd4f8895&q=${this.props.searchQuery}&image_type=photo&page=${this.state.page}&per_page=12`)
-//                     .then(res => res.json())
-//                     .then(data =>
-//                         this.setState(prevState => ({
-//                             articles:
-//                                 this.state.page === 1 ? data.hits : [...prevState.articles, ...data.hits],
-//                             totalPages: Math.ceil(data.totalHits / 12)
-//                         }))
-//                     )
-//                     .finally(() => this.setState({ isLoading: false }))
-//                 }, 250)
-//         } 
-//     }
-//     loadMore = () => {
-//         this.setState(prevState => ({
-//             page: prevState.page + 1
-//         }))
-//     }
-
-//     render() {
-//         return (<>
-//             <ul className="gallery">
-//                 {!this.state.totalPages && <h1 className="errorMsg">can't find images by name '{this.props.searchQuery}'</h1>}
-//                 {this.state.articles.length !== 0 && this.state.articles.map(({ id, webformatURL, tags, largeImageURL }) => (
-//                     <ImageGalleryItem
-//                         key={id}
-//                         src={webformatURL}
-//                         name={tags}
-//                         modalSrc={largeImageURL}
-//                     />
-//                 ))}
-//             </ul>
-//             {this.state.isLoading && <Loader/>}
-//             {this.state.articles.length > 0 && this.state.totalPages > this.state.page && !this.state.isLoading &&(<Button loadMore={this.loadMore} />)}
-            
-            
-//         </>
-//         )
-//     }
-// }
-
-// export default ImageGallery
 
 ImageGallery.propTypes = {
     requestImg: PropTypes.string.isRequired,
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,3 @@
-// import React, { Component } from "react";
 import  { useEffect } from "react";
 import { createPortal } from 'react-dom'
 import PropTypes from 'prop-types'
@@ -34,43 +33,6 @@ export default function Modal({ name, modalSrc, onClose }) {
         modalRoot,
     ) 
 }
-
-
-
-// class Modal extends Component {
-
-//     componentDidMount() {
-//         window.addEventListener('keydown', this.handleKeyDown)
-//     }
-//     componentWillUnmount() {
-//         window.removeEventListener('keydown', this.handleKeyDown)
-//     }
-    
-//     handleKeyDown = e => {
-//         if (e.code === 'Escape') {
-//             this.props.onClose();
-//         }
-//     }
-//     handleBackdropClick = e => {
-//         if (e.currentTarget !== e.target) {
-//             this.props.onClose()
-//         }
-//     }
-
-//     render() {
-//         const {modalSrc, name} = this.props
-//           return  createPortal(
-//         <div className="overlay" onClick={this.handleBackdropClick}>
-//             <div className="modal">
-//                 <img src={modalSrc} alt={name} />
-//             </div>
-//         </div>,
-//               modalRoot,
-//     )  
-//     }
-// }
-
-// export default Modal
 
 Modal.propTypes = {
     onClose: PropTypes.func.isRequired,
